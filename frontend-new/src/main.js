24942ea347f5d4c093323c48cb76f5c796153b09
tribalcz <tribal.cz@gmail.com>
2025-11-11 21:20:36 +0100

    Testing the automatic application discovery function

--- frontend-new/src/main.js
+++ frontend-new/src/main.js
@@ -15,11 +15,16 @@ import { Store } from '@core/Store.js';
 import { TaskBar } from '@components/TaskBar.js';
 import { StartMenu } from '@components/StartMenu.js';
 import { Clock } from '@components/Clock.js';
-import {AppManager} from "@core/AppManager.js";
+import { AppManager } from "@core/AppManager.js";
 
+import ProcessMonitor from "@apps/process-monitor/index.js";
+import { Index } from '@apps/fileExplorer/index.js';
+import Welcome from '@apps/welcome/index.js';
+
+import {getIcon} from "@utils/Icons.js";
 console.log('WebDesk WM starting...');
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {  // ✅ async
     console.log('Init EventBus...');
     const eventBus = new EventBus();
 
@@ -41,6 +46,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     console.log('Init AppManager...');
     const appManager = new AppManager(eventBus, store);
+
+    console.log('Running app discovery...');
+    await appManager.discovery();
+
+    console.log('Discovered apps:', appManager.registry.getAll().map(a => a.id).join(', '))
 
     const taskBar = new TaskBar(windowManager);
     const startMenu = new StartMenu(windowManager);
@@ -64,4 +74,37 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('Event:', data);
         });
     }
+
+    const ctx = {
+        windowManager: webdesk.windowManager,
+        eventBus: webdesk.eventBus,
+        store: webdesk.store,
+        manifest: { id: 'file-explorer', window: { defaultWidth: 700, defaultHeight: 500 }}
+    };
+    const fe = new Index(ctx);
+    await fe.init();
+    await fe.open();
+
+    // Process Monitor
+    fetch('/src/apps/process-monitor/meta/manifest.json')
+        .then(r => r.json())
+        .then(m => console.log('✅ Process Monitor manifest:', m));
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+
+    const ctxpm = {
+        windowManager: webdesk.windowManager,
+        eventBus: webdesk.eventBus,
+        store: webdesk.store,
+        appManager: webdesk.appManager,
+        manifest: {
+            id: 'process-monitor',
+            window: { defaultWidth: 650, defaultHeight: 750, persistent: false }
+        }
+    };
+
+    const pm = new ProcessMonitor(ctxpm);
+    await pm.init();
+    await pm.open();
+
 });
